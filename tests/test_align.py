0d8306cd47ec406a8848d2a95e98d090e68e09e7
mdlpstsci <mdelapena@stsci.edu>
2018-10-25 17:10:21 -0400

    [WIP] Created test to process a random list of datasets (#11)
    * [WIP] Created test to process a random list of datasets
    
    * Improvements
    
    * Updates to process random datasets.
    
    * Mark slow/xfail test and cleanup
    
    * [WIP] Fixed bad typo which caused unpredictable behavior

--- tests/test_align.py
+++ tests/test_align.py
@@ -1,11 +1,13 @@
 import os
 import pytest
+import numpy
 from astropy.table import Table
 
 from stwcs import updatewcs
 
 from base_test import BaseHLATest
 from hlapipeline import align_to_gaia
+import hlapipeline.utils.catalog_utils as catutils
 
 @pytest.mark.bigdata
 class TestAlignMosaic(BaseHLATest):
@@ -86,3 +88,164 @@ class TestAlignMosaic(BaseHLATest):
         rms_y = max(shift_file['col7'])
 
         assert (rms_x <= 0.25 and rms_y <= 0.25)
+
+    @pytest.mark.xfail
+    @pytest.mark.slow
+    def test_align_randomFields(self):
+        """ Wrapper to set up the test for aligning a large number of randomly
+            selected fields (aka datasets) from a input ascii file (CSV).
+    
+            The wrapper provides the parameter settings for the underlying test, 
+            as well as implements the criterion for the overall success or failure
+            of the test.
+        """
+        inputListFile = 'ACSList50.csv'
+        
+        # Desired number of random entries for testing
+        inputNumEntries = 50
+
+        # Seed for random number generator
+        inputSeedValue = 1
+
+        # Obtain the full path to the file containing the dataset field names
+        self.input_loc  = 'master_lists'
+        self.curdir     = os.getcwd()
+        input_file_path = self.get_data(inputListFile)
+
+        # Randomly select a subset of field names (each field represented by a row) from 
+        # the master CSV file and return as an Astropy table
+        randomCandidateTable = catutils.randomSelectFromCSV(input_file_path[0], 
+            inputNumEntries, inputSeedValue)
+        print('table:', randomCandidateTable)
+
+        # Invoke the methods which will handle acquiring/downloading the data from 
+        # MAST and perform the alignment
+        percentSuccess = 0.0
+        try:
+            percentSuccess = self.align_randomFields (randomCandidateTable)
+        except Exception:
+            pass
+
+        assert(percentSuccess >= 0.70)
+
+    @pytest.mark.xfail
+    def test_align_fewRandomFields(self):
+        """ Wrapper to set up the test for aligning a *FEW* randomly
+            selected fields (aka datasets) from a input ascii file (CSV).
+    
+            The wrapper provides the parameter settings for the underlying test, 
+            as well as implements the criterion for the overall success or failure
+            of the test.
+        """
+        inputListFile = 'ACSList5.csv'
+        
+        # Desired number of random entries for testing
+        inputNumEntries = 5
+
+        # Seed for random number generator
+        inputSeedValue = 1
+
+        # Obtain the full path to the file containing the dataset field names
+        self.input_loc  = 'master_lists'
+        self.curdir     = os.getcwd()
+        input_file_path = self.get_data(inputListFile)
+
+        # Randomly select a subset of field names (each field represented by a row) from 
+        # the master CSV file and return as an Astropy table
+        randomCandidateTable = catutils.randomSelectFromCSV(input_file_path[0], 
+            inputNumEntries, inputSeedValue)
+        print('table:', randomCandidateTable)
+
+        # Invoke the methods which will handle acquiring/downloading the data from 
+        # MAST and perform the alignment
+        percentSuccess = 0.0
+        try:
+            percentSuccess = self.align_randomFields (randomCandidateTable)
+        except Exception:
+            pass
+
+        assert(percentSuccess >= 0.70)
+
+    def align_randomFields(self, randomTable):
+        """ Process randomly selected fields (aka datasets) stored in an Astropy table.
+
+            Each field is used as input to determine if it can be aligned to an 
+            astrometric standard.  The success or fail status for each test is retained
+            as the overall success or fail statistic is the necessary output from
+            this test.
+        """
+
+        numSuccess = 0
+        numAllDatasets = 0
+
+        # Read the table and extract a list of each dataset name in IPPSSOOT format
+        # which is either an association ID or an individual filename
+        dataset_list = get_dataset_list(randomTable)
+
+        numAllDatasets = len(dataset_list)
+
+        # Process the dataset names in the list
+        #
+        # If the dataset name represents an association ID, the multiplicity 
+        # of images within the association need to be processed.  Otherwise,
+        # the dataset is a single image.
+        #
+        # If the "alignment" of a field/dataset fails for any reason, trap
+        # the exception and keep going.
+        for dataset in dataset_list:
+
+           print(dataset)
+           try:
+               shift_file = self.run_align([dataset])
+               x_shift = numpy.alltrue(numpy.isnan(shift_file['col2']))
+               rms_x = max(shift_file['col6'])
+               rms_y = max(shift_file['col7'])
+
+               if not x_shift and ((rms_x <= 0.25) and (rms_y <= 0.25)):
+                   numSuccess += 1
+
+           # Catch anything that happens as this dataset will be considered a failure, but
+           # the processing of datasets should continue
+           except Exception:
+               pass
+
+        # Determine the percent success over all datasets processed
+        percentSuccess = numSuccess/numAllDatasets
+        print('Number of successful tests: ', numSuccess, ' Total number of tests: ', numAllDatasets, ' Percent success: ', percentSuccess*100.0)
+ 
+        return percentSuccess
+
+def get_dataset_list(tableName):
+    """ Standalone function to read the Astropy table and get the dataset names
+
+    Parameters
+    ==========
+    tableName : str
+        Filename of the input master CSV file containing individual 
+        images or association names, as well as observational 
+        information regarding the images
+
+    Returns
+    =======
+    datasetNames: list
+        List of individual image or association base (IPPSSOOT) names
+    """
+
+    #dataFromTable = Table.read(filename, format='ascii')
+    datasetIDs = tableName['observationID']
+    asnIDs     = tableName['asnID']
+
+    datasetNames = []
+
+    # Determine if the data is part of an association or is an individual image
+    for imgid,asnid in zip(datasetIDs,asnIDs):
+
+        # If the asnID is the string NONE, this is an individual image,
+        # and it is necessary to get the individual image dataset name.  
+        # Otherwise, this is an association dataset, so just add the asnID.
+        if (asnid.upper() == "NONE"):
+            datasetNames.append(imgid)
+        else:
+            datasetNames.append(asnid)
+
+    return datasetNames
