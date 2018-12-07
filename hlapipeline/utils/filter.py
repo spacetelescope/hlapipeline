01bcfdab1639d919f7d5448b5b68b9fe42f9c7de
mdlpstsci <mdelapena@stsci.edu>
2018-12-07 13:48:10 -0500

    Jira hla 120 - Evaluate the viability of a dataset for alignment processing (#35)
    * [WIP] Function to filter datasets to determine viability for processing
    
    * Updated filter routine
    
    * Fixed access to SCAN_TYP which is a WFC3 keyword only.
    
    * Updated filter.py for improvements per review.
    
    * Improved filter.py for various filter FITS keywords.

--- hlapipeline/utils/filter.py
+++ hlapipeline/utils/filter.py
@@ -13,7 +13,6 @@ import math
 
 __all__ = ['analyze_data']
 
-#def analyze_data(inputFileList, optionalCols = None):
 def analyze_data(inputFileList, **kwargs):
     """
     Determine if images within the dataset can be aligned
@@ -31,26 +30,34 @@ def analyze_data(inputFileList, **kwargs):
         Astropy Table object containing data pertaining to the associated dataset, including 
         the doProcess bool
 
+    Notes 
+    =====
     The keyword/value pairs below define the "cannot process categories".
-    OBSTYPE: not IMAGING
-    MTFLAG: T
-    SCAN-TYP: C or D (or !N)
-    FILTER: G*, *POL*
-    APERTURE: *GRISM*, G*-REF, RAMP, *POL*
-    TARGNAME: DARK, TUNGSTEN, BIAS, FLAT, EARTH-CALIB, DEUTERIUM
-    EXPOTIME: 0 
+    OBSTYPE : not IMAGING
+    MTFLAG : T
+    SCAN-TYP : C or D (or !N)
+    FILTER : G*, *POL*
+    FILTER1 : G*, *POL*
+    FILTER2 : G*, *POL*
+    APERTURE : *GRISM*, G*-REF, RAMP, *POL*
+    TARGNAME : DARK, TUNGSTEN, BIAS, FLAT, EARTH-CALIB, DEUTERIUM
+    EXPOTIME : 0 
+
+    Keywords only for WFC3 data: SCAN_TYP and FILTER
+    Keywords only for ACS data: FILTER1 and FILTER2
     """
     OBSKEY = 'OBSTYPE'
     MTKEY  = 'MTFLAG'
     SCNKEY = 'SCAN_TYP'
     FILKEY = 'FILTER'
+    FILKEY1 = 'FILTER1'
+    FILKEY2 = 'FILTER2'
     APKEY  = 'APERTURE'
     TARKEY = 'TARGNAME'
     EXPKEY = 'EXPTIME'
     DATEKEY = 'DATE-OBS'
 
-    # Create an astropy table
-    #optionalCols = kwargs.get('optionalCols', None)
+    filtNameList = [FILKEY1, FILKEY2]
 
     catalog = None
     foundSources = 0 
@@ -58,17 +65,20 @@ def analyze_data(inputFileList, **kwargs):
     rms_y = None
     matchSources = 0 
     isSuccess = False
-    #isSuccess = 0
     dateObs = None
     namesArray = ('imageName', 'instrument', 'detector', 'filter', 'aperture', 'obstype', 
             'subarray', 'dateObs', 'doProcess', 'catalog', '# found sources', '# match sources', 
             'rms_x', 'rms_y', 'isSuccess')
-    #dataType = ('S20', 'S20', 'S20', 'S20', 'S20', 'S20', 'i1', 'S20', 'i1', 'S20', 'i4', 'i4', 'f8', 'f8', 'i1')
     dataType = ('S20', 'S20', 'S20', 'S20', 'S20', 'S20', 'b', 'S20', 'b', 'S20', 'i4', 'i4', 'f8', 'f8', 'b')
 
+    # Create an astropy table
     outputTable = Table(names=namesArray,dtype=dataType)
 
     # Loop over the list of images to determine viability for alignment processing
+    # Capture the data characteristics before any evaluation so the information is
+    # available for the output table regardless of which keyword is used to 
+    # to determine the data is not viable for alignment.
+
     for inputFile in inputFileList:
 
         header_hdu  = 0
@@ -76,18 +86,38 @@ def analyze_data(inputFileList, **kwargs):
 
         obstype  = (header_data[OBSKEY]).upper()
         mtflag   = (header_data[MTKEY]).upper()
-        scan_typ = (header_data[SCNKEY]).upper()
-
-        sfilter  = (header_data[FILKEY]).upper()
-        aperture = (header_data[APKEY]).upper()
-        targname = (header_data[TARKEY]).upper()
-        expotime = header_data[EXPKEY]
 
         # Keywords to use potentially for analysis
         instrume = (header_data['INSTRUME']).upper()
         detector = (header_data['DETECTOR']).upper()
         subarray = header_data['SUBARRAY']
         dataObs  = header_data['DATE-OBS']
+        
+        scan_typ = ''
+        if instrume == 'WFC3':
+            scan_typ = (header_data[SCNKEY]).upper()
+
+        sfilter = ''
+        if instrume == 'WFC3':
+            sfilter  = (header_data[FILKEY]).upper()
+        # Concatenate the two ACS filter names together with an underscore
+        # If the filter name is blank, skip it
+        if instrume == 'ACS':
+            for filtname in filtNameList:
+
+                # The filter keyword value could be zero or more blank spaces 
+                # Strip the name down to non-zero leading or trailing blanks
+                if len(header_data[filtname].upper().strip()) > 0:
+
+                    # If the current filter variable already has some content,
+                    # need to append an underscore before adding more text
+                    if len(sfilter) > 0:
+                        sfilter += '_'
+                    sfilter += header_data[filtname].upper().strip()
+
+        aperture = (header_data[APKEY]).upper()
+        targname = (header_data[TARKEY]).upper()
+        expotime = header_data[EXPKEY]
 
         # Determine if the image has one of these conditions.  The routine
         # will exit processing upon the first satisfied condition.
@@ -105,25 +135,33 @@ def analyze_data(inputFileList, **kwargs):
             noProcKey   = MTKEY
             noProcValue = mtflag 
 
-        # Bostrophidon without or with dwell
+        # Bostrophidon without or with dwell (WFC3 only)
         elif any ([scan_typ == 'C', scan_typ == 'D']):
             noProcKey   = SCNKEY
             noProcValue = scan_typ
 
         # Filter which begins with !F (e.g., 'POL*' or 'G*')
-        elif sfilter[0] != 'F': 
+        # The sfilter variable may be the concatenation of two filters (f160_clear) - 
+        # look at the first character of each filter to see if the character is !F
+        elif sfilter[0] != 'F' and sfilter[0] != '' and sfilter[0] != 'C' : 
             noProcKey   = FILKEY
             noProcValue = sfilter
 
+        elif '_' in sfilter:
+            pos = sfilter.index('_')
+            pos += 1
+
+            if sfilter[pos] != 'F' and sfilter[pos] != '' and sfilter[pos] != 'C' : 
+                noProcKey   = FILKEY
+                noProcValue = sfilter
+
         # Ramp, polarizer, or grism 
-        elif any (['RAMP' in aperture, 'POL' in aperture, 'GRISM' in aperture, 
-                  '-REF' in aperture]):
+        elif any (x in aperture for x in ['RAMP', 'POL', 'GRISM', '-REF']):
             noProcKey   = APKEY
             noProcValue = aperture 
 
         # Calibration target
-        elif any (['DARK' in targname, 'TUNG' in targname, 'BIAS' in targname, 
-                  'FLAT' in targname, 'DEUT' in targname, 'EARTH-CAL' in targname]):
+        elif any (x in targname for x in ['DARK', 'TUNG', 'BIAS', 'FLAT', 'DEUT', 'EARTH-CAL']):
             noProcKey   = TARKEY
             noProcValue = targname
 
@@ -133,11 +171,12 @@ def analyze_data(inputFileList, **kwargs):
             noProcValue = expotime 
 
         if (noProcKey is not None):
-            #doProcess = 0
             doProcess = False
 
-        # Issue message to log file 
-        #issue_msg(inputFileList, noProcKey, noProcValue)
+            # Issue message to log file - reports the first issue which makes the file ineligible
+            # for alignment
+            issue_msg(inputFile, noProcKey, noProcValue)
+
 
         # Populate a row of the table
         outputTable.add_row([inputFile, instrume, detector, sfilter, aperture, obstype, 
@@ -153,6 +192,6 @@ def issue_msg(filename, key, value):
         with alignment.
     """
 
-    print('Dataset ' + filename + ' has keyword = value of ' + key + ' = ' + str(value) + '.\n')
+    print('\nDataset ' + filename + ' has (keyword = value) of (' + key + ' = ' + str(value) + ').')
     print('Dataset cannot be aligned.\n')
 
