b8e98873fcc7fa2ec33089d1c41428cb602bb93e
Warren Hack <hack@stsci.edu>
2018-12-20 14:40:10 -0500

    Turn back on a test

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -45,7 +45,7 @@ class TestPipeline(BaseHLATest):
                                  ('J8D806010','j8d806bgq_sky_cat.ecsv'),
                                  ('IB2V09010', 'ib2v09kzq_sky_cat.ecsv')]
                             )
-    def XXtest_generate_catalog(self,input_filenames, truth_file):
+    def test_generate_catalog(self,input_filenames, truth_file):
         """ Verify whether sources from astrometric catalogs can be extracted from images.
 
         Success Criteria
@@ -140,7 +140,7 @@ class TestPipeline(BaseHLATest):
             for group_id,image in enumerate(local_files):
                 imglist.extend(amutils.build_nddata(image, group_id,
                                                     source_catalogs[image]['catalog_table']))
-                                                
+
             # Specify matching algorithm to use
             match = tweakwcs.TPMatch(searchrad=250, separation=0.1,
                                      tolerance=5, use2dhist=True)
