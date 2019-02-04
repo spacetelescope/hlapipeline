a0f0676f93783b4da5e13619da76aa8762a59845
Michael Dulude <dulude@stsci.edu>
2019-02-04 12:32:12 -0500

    hlapipeline/utils/filter.py: increased 'processMsg' text string length from 20 characters to 30 characters

--- hlapipeline/utils/filter.py
+++ hlapipeline/utils/filter.py
@@ -104,7 +104,7 @@ def analyze_data(inputFileList, **kwargs):
             'subarray', 'dateObs', 'mjdutc', 'doProcess', 'processMsg', 'catalog', 'foundSources',
             'catalogSources','matchSources', 'rms_x', 'rms_y', 'rms_ra', 'rms_dec', 'completed',
             'fit_rms', 'total_rms', 'datasetKey', 'status', 'headerletFile')
-    dataType = ('S20', 'S20', 'S20', 'S20', 'S20', 'S20', 'b', 'S20', 'f8', 'b', 'S20',
+    dataType = ('S20', 'S20', 'S20', 'S20', 'S20', 'S20', 'b', 'S20', 'f8', 'b', 'S30',
             'S20', 'i4', 'i4', 'i4', 'f8', 'f8', 'f8', 'f8', 'b', 'f8', 'f8', 'i8', 'i4', 'S30')
 
     # Create an astropy table
